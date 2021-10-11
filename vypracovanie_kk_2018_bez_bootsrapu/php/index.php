2884bb1dabacc73bdba21f3b1783c30c9bf0dea9
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2021-10-11 21:20:46 +0200

    Starting Zenit kk 2018

---
+++ vypracovanie_kk_2018_bez_bootsrapu/php/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Krajské kolo 2018</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.php" class="active" title="domov">Domov</a>
+            <a href="" title="sluzby">Služby</a>
+            <a href="" title="nasa_praca">Naša práca</a>
+            <a href="" title="rezervacky">Rezervačky</a>
+        </nav>
+    </header>
+
+    <div class="uvod">
+        <div>
+            <img src="../obrazky/pozadie-uvod.jpg" alt="uvod" title="uvod">
+        </div>
+        <div class="obsah_na_fotke">
+            <img src="../obrazky/logo.png" alt="uvod" title="uvod">
+            <h1 class="nazov">ZEN & HAR</h1>
+            <p>Váš špecialista na veterány</p>
+        </div>
+    </div>
+
+    <div class="sluzby">
+        <h1 class="sluzby_nadpis">SLUŽBY</h1>
+        <hr class="sluzby_ciara">
+        <div class="sluzby_vypisane">
+            <div class="sluzba">
+                <img src="../obrazky/sluzby-opravy.png" alt="uvod" title="uvod">
+                <p class="title_sluzby">OPRAVY</p>
+                <p class="popis_Sluzby">Všeobecné opravy Špecializované opravy Dobové náhradné diely</p>
+            </div>
+            <div class="sluzba">
+                <img src="../obrazky/sluzby-natery.png" alt="uvod" title="uvod">
+                <p class="title_sluzby">Čalúnenie </p>
+                <p class="popis_Sluzby">Veľký rozsah tkanín Vrátane dobových koží Široká paleta farieb</p>
+            </div>
+            <div class="sluzba">
+                <img src="../obrazky/sluzby-natery.png" alt="uvod" title="uvod">
+                <p class="title_sluzby">NÁTERY</p>
+                <p class="popis_Sluzby">Striekanie Ručné malovanie na základe požiadavky klienta</p>
+            </div>
+        </div>
+    </div>
+
+
+
+    <footer>
+        <div class="caption">
+            <p>2021 vytvoril Miroslav Hanisko SPŠT
+            </p>
+            <p>© všetky práva vyhradené tím ZENIT
+            </p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
